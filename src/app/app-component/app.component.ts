a45229fc46ee2e2a8e1201c90f4b53f89a1ab91d
MariaSilvaPeres <maria.silva.peres@hotmail.com>
2021-03-25 13:01:31 +0000

    finalize

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { without } from 'lodash';
+import { without, findIndex } from 'lodash';
+import { AppRoutingModule } from '../app-routing.module';
 
 library.add(faTimes, faPlus);
 
@@ -11,22 +12,88 @@ library.add(faTimes, faPlus);
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  title = 'appointment-interface';
   theList: object[] = [];
+  modifiedList: object[] = [];
+  orderBy: string;
+  orderType: string;
+  lastIndex!: number;
 
-  addApt(theApt: object) {
+  addApt(theApt: any) {
+    theApt.aptId = this.lastIndex;
     this.theList.unshift(theApt);
+    this.modifiedList.unshift(theApt);
+    this.lastIndex++;
   }
 
   deleteApt(theApt: object) {
     this.theList = without(this.theList, theApt);
+    this.modifiedList = without(this.theList, theApt);
   }
 
-  constructor(private http: HttpClient) {}
+  // searchApt(theQuery: string) {
+  //   this.theList = this.theList.filter((eachItem) => {
+  //     return (
+  //       eachItem['petName'].toLowerCase().includes(theQuery.toLowerCase()) ||
+  //       eachItem['ownerName'].toLowerCase().includes(theQuery.toLowerCase()) ||
+  //       eachItem['aptNotes'].toLowerCase().includes(theQuery.toLowerCase())
+  //     );
+  //   });
+  // }
+
+  sortItems() {
+    let order: number;
+    if (this.orderType === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    // this.modifiedList.sort((a, b) => {
+    //   if (
+    //     a[this.orderBy].toLowerCase() < b[this.orderBy].toLowerCase()
+    //   ) {
+    //     return -1 * order;
+    //   }
+    //   if (
+    //     a[this.orderBy].toLowerCase() > b[this.orderBy].toLowerCase()
+    //   ) {
+    //     return 1 * order;
+    //   }
+    // });
+  }
+
+  updateApt(aptInfo: {
+    theApt: { aptId: number };
+    labelName: string | number;
+    newValue: number;
+  }) {
+    let aptIndex: number;
+    let modifiedIndex: number;
+
+    aptIndex = findIndex(this.theList, { aptId: aptInfo.theApt.aptId });
+    modifiedIndex = findIndex(this.modifiedList, {
+      aptId: aptInfo.theApt.aptId,
+    });
+
+    // this.theList[aptIndex][aptInfo.labelName] = aptInfo.newValue;
+    // modified.theList[modifiedIndex][aptInfo.labelName] = aptInfo.newValue;
+  }
+
+  constructor(private http: HttpClient) {
+    this.orderBy = 'petName';
+    this.orderType = 'asc';
+  }
 
   ngOnInit(): void {
+    this.lastIndex = 0;
     this.http.get<Object[]>('../assets/data.json').subscribe((data) => {
-      this.theList = data;
+      this.theList = data.map((item: any) => {
+        item.aptId = this.lastIndex++;
+        return item;
+      });
+      this.modifiedList = data;
+      this.sortItems();
+      console.log(this.modifiedList);
     });
   }
 }
--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
 })
 export class SearchComponent implements OnInit {
+  @Output() queryEvt = new EventEmitter<string>();
+
+  query: string | undefined; 
+
+  handleQuery(query: string | undefined) {
+    this.queryEvt.emit(query);
+  }
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
